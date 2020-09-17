f825ddf0f805c822e1bedad51d23398414d54fdb
unknown <romanna.d@gmail.com>
2020-09-17 13:18:49 +0300

    added slides styles for sliderAbout

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ let nav_link = document.querySelectorAll('.header__link');
 function toggleActive(){
 	burger.classList.toggle('active');
 	 nav.classList.toggle('active');
-	 body.classList.toggle('lock');
+	 //body.classList.toggle('lock');
 };
 burger.addEventListener('click', toggleActive);
 
@@ -28,7 +28,43 @@ function hideBurgerNavigation(el){
 	function removeActive(){
 		burger.classList.remove('active');
 		nav.classList.remove('active');
-		body.classList.remove('lock');
+		//body.classList.remove('lock');
 	};
 };
 hideBurgerNavigation();
+
+//get styles for focused slide (for sliderAbout)
+let slides = document.querySelectorAll('.about__slide');
+console.log(slides);
+function changeSlideStyle(el){
+	slides.forEach(el => {
+		el.addEventListener('click', toggleVisible)
+	});
+	function toggleVisible(){
+		console.log(this);
+		let mask = this.firstElementChild.firstElementChild.nextElementSibling;
+		let list = mask.nextElementSibling;
+		mask.classList.toggle('visible');
+		list.classList.toggle('visible'); 
+	};
+};
+changeSlideStyle();
+
+
+
+
+//sliders management
+/*$(document).ready(function(){
+	$('.sliderAbout').slick({
+		//dots: true
+	});
+});*/
+
+$(document).ready(function(){
+	$('.sliderClients').slick({
+		arrows: false,
+		dots: true
+	});
+});
+
+
